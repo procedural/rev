f91ab7cc205cc652f50edf439a1f53d22a2f5f2a
procedural <iamvfx@gmail.com>
2020-09-21 13:15:04 +0300

    Add revTerminate

--- rev.h
+++ rev.h
@@ -4,3 +4,4 @@ void revTexture(unsigned textureId, const char * pathToPPM);
 void revOffset(float positionX, float positionY, float positionZ, float rotationQuaternionX, float rotationQuaternionY, float rotationQuaternionZ, float rotationQuaternionW);
 void revVertex(float x, float y, float z, float u, float v, unsigned textureId, unsigned visualEffectId);
 void revPresent(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
+void revTerminate(void);
