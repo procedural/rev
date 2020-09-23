854ee4dc84eea9c56b3c1cab8e9ba005ffb7fd30
procedural <iamvfx@gmail.com>
2020-09-23 12:31:20 +0300

    Update rev.h

--- rev.h
+++ rev.h
@@ -4,5 +4,4 @@ void revTexture(unsigned textureId, const char * pathToPPM);
 void revVertex(unsigned vertexIndex, float x, float y, float z, float u, float v);
 void revOffset(float positionX, float positionY, float positionZ, float rotationQuaternionX, float rotationQuaternionY, float rotationQuaternionZ, float rotationQuaternionW);
 void revDraw(unsigned vertexIndexStart, unsigned vertexIndexEnd, unsigned textureId, unsigned visualEffectId);
-void revPresent(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
-void revTerminate(void);
+void revPresent(const void * displayOrHinstance, const void * windowOrHwnd, unsigned windowWidth, unsigned windowHeight, unsigned terminate);
