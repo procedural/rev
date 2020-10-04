142930d1d8bce8c7b45d7be766f815cd875510b8
procedural <iamvfx@gmail.com>
2020-10-04 16:14:47 +0300

    Replace revTexture with revTexel

--- rev.h
+++ rev.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void revTexture(unsigned textureId, const char * pathToPPM);
+void revTexel(unsigned textureId, unsigned textureSize, unsigned texelIndexX, unsigned texelIndexY, float r, float g, float b, float a);
 void revVertex(unsigned vertexIndex, float x, float y, float z, float u, float v);
 void revOffset(float positionX, float positionY, float positionZ, float rotationQuaternionX, float rotationQuaternionY, float rotationQuaternionZ, float rotationQuaternionW);
 void revDraw(unsigned vertexIndexStart, unsigned vertexIndexEnd, unsigned textureId, unsigned visualEffectId, float * visualEffectParameters);
